04b7af4ae0653985bf82f2d8ab14741d8684ac5a
Natalija_L <natalija.liske@gmail.com>
2023-06-23 12:27:28 +0300

    main

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const buttonDOM = document.getElementById('buttonRock');
+console.log(buttonDOM);
+
+buttonDOM.addEventListener('click', () => {
+    console.log(buttonDOM);
+});
+
+const rock = buttonDOM.innerText;
+
+console.log(rock);
 
 
 
